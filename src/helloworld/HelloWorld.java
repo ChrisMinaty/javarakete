f38d9e7840cc4e8d6549cfe407eedd84024cd155
CM77 <29871766+CM77@users.noreply.github.com>
2026-03-01 19:07:45 +0100

    Tutorial 001 - Packages und Klassen, main-Methode und Hello World ausgeben

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,16 @@
+package helloworld;
+
+public class HelloWorld {
+
+	// Tutorial 001 - https://youtu.be/IO_GS16CuIw
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Hello World1");
+		System.out.println("Hello World2");
+		System.out.println(1);
+		System.out.println(false);
+		System.out.println(1.2);
+	}
+
+}
