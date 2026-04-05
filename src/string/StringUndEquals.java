8aa99f83a7edc828147ed1060986437ea21f70b1
CM77 <29871766+CM77@users.noreply.github.com>
2026-04-05 18:00:43 +0200

    Tutorial 006: Login-Vergleich mit String mit == bzw. equals

---
+++ src/string/StringUndEquals.java
@@ -0,0 +1,34 @@
+package string;
+
+import java.util.Scanner;
+
+public class StringUndEquals {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String username = "admin";
+		String input = scanner.nextLine();
+
+		// Achtung!
+		// username → "admin" (Objekt A)
+		// input → "admin" (Objekt B)
+
+		if (input == username) {
+			System.out.println("Login erfolgreich!");
+		} else {
+			System.out.println("Falscher Benutzername!");
+		}
+
+		if (input.equals(username)) { // equals() schaut in den String und vergleicht Zeichen für Zeichen.
+			System.out.println("Login erfolgreich!");
+		} else {
+			System.out.println("Falscher Benutzername!");
+		}
+		if (input.equalsIgnoreCase(username)) { // equalsIgnoreCase ist nicht case-sensitiv: nimmt etwa auch "Admin"
+			System.out.println("Login erfolgreich!");
+		} else {
+			System.out.println("Falscher Benutzername!");
+		}
+	}
+
+}
